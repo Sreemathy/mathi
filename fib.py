bf8cfdf3c3fa28b70768bfba76413da09d26df88
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-10 11:20:08 +0530

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,13 @@
+def fib(n):
+   if n <= 1:
+       return n
+   else:
+       return(fib(n-1) + fib(n-2))
+
+nterms = 5
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(fib(i))

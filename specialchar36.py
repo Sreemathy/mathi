f41c1ce2fdf87e3bdbe17ef1c0765e3159ce55e3
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-17 11:34:31 +0530

    Create specialchar36.py

---
+++ specialchar36.py
@@ -0,0 +1,9 @@
+s = 'kavi143:mathy'
+numbers = sum(c.isdigit() for c in s)
+words   = sum(c.isalpha() for c in s)
+spaces  = sum(c.isspace() for c in s)
+others  = len(s) - numbers - words - spaces
+print(others)
+
+
+

3a282b4335c48cc92b3fbc22f90e05dfa450ddaf
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-17 14:15:17 +0530

    Update max59.py

--- max59.py
+++ max59.py
@@ -1,7 +1,7 @@
 a=[]
-n=int(input("Enter number of elements:"))
-for i in range(1,n+1):
-    b=int(input("Enter element:"))
-    a.append(b)
+b=int(input(" number of elements")
+for i in range(1,1+b):
+      c=int(input("enter a number")
+        a.append(b)
 a.sort()
-print("Largest element is:",a[n-1])
+print("the maximum",a[n-1])

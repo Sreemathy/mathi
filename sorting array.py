944b29841509662a5eddc0decdbd344e58724aa6
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-17 10:51:38 +0530

    Create sorting array.py

---
+++ sorting array.py
@@ -0,0 +1,12 @@
+num = int(input("How many numbers : ")) 
+storage = []
+result = []
+for i in range(1,num+1):
+    a = int(input("Enter value" + str(i) + " : "))
+    storage.append(a) 
+for m in range(len(storage)):
+    b = min(storage)
+    storage.remove(b)
+    result.append(b) 
+j = ' '.join(str(i) for i in result)
+print(j)

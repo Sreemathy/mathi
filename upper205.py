0719156ff6e55c402c2b8c86642f093e86f119a4
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-22 15:54:45 +0530

    Create upper205.py

---
+++ upper205.py
@@ -0,0 +1,10 @@
+name = 'sreMAthy'
+list = []
+for i in name:
+    if i.isupper():
+        list.append(i.lower())
+    elif i.islower():
+        list.append(i.upper())
+    else:
+        list.append(i)
+print (''.join(list))

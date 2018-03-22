bb7d489ec87b5fa838c080f4bb14f3e009e1c36b
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-22 16:19:18 +0530

    Create strcmpcase55.py

---
+++ strcmpcase55.py
@@ -0,0 +1,6 @@
+string1 = input("Enter string1: ")
+string2 = input("Enter string2: ")
+if string1.lower() == string2.lower():
+		print("Both Strings are Equal.\n")
+else:
+		print("Strings are Not Equal.\n")

6708979546f879064fe0e7088efd54d0d0245e00
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-03-24 15:00:34 +0530

    Create 186substring.py

---
+++ 186substring.py
@@ -0,0 +1,7 @@
+string=input("Enter string:")
+substr=input("Enter word:")
+substr2=input("enter word")
+if((string.find(substr)==-1) and(string.find(substr2==-1))):
+      print("Substring is not present")
+else:
+      print("Substring in present")

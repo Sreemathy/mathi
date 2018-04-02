a93fc0964b24c6d690b2e2a3b7cfc178ef8886cb
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-04-02 10:13:40 +0530

    Create 112number.py

---
+++ 112number.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number"))
+i=int(input("enter a number"))
+if n in range(0,i):
+    print( " %s is in the range"%str(n))
+else :
+    print("The number is outside the given range.")

0df2755762afbcc8d290ba67a72732b57f8e50cc
Sreemathy <36299211+Sreemathy@users.noreply.github.com>
2018-05-24 11:19:21 +0530

    Create h3.py

---
+++ h3.py
@@ -0,0 +1,13 @@
+l=[]
+s=[]
+a=int(input())
+for i in range(0,a):
+  n=int(input())
+  l.append(n)
+for i in l:
+  a=l.index(i)
+  if(a==i):
+    print(i)
+    s.append(i)
+print("".join(str(i) for i in s))
+    
